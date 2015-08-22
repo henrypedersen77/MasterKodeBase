587ab833bab9d9e5cdba570cc3fb93abd8c26aa4
henrypedersen77 <henrypedersen77@gmail.com>
2015-08-22 15:34:02 +0200

    Produkt billede håndtering

--- webshop_mvc_basis/Controllers/ImageController.cs
+++ webshop_mvc_basis/Controllers/ImageController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace webshop_mvc_basis.Controllers
@@ -20,6 +22,23 @@ namespace webshop_mvc_basis.Controllers
             }
 
             return FileNames;
+        }
+
+        //Returnere Produkt billedet råt, skal måske måske ikke bruges?
+        public HttpResponseMessage Post([FromBody]string billedeNavn){
+            Image image = Image.FromFile(System.Web.Hosting.HostingEnvironment.MapPath("~/Content/Images/Products/") + billedeNavn);
+           
+            MemoryStream memoryStream = new MemoryStream();
+            
+            image.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+            HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
+
+            result.Content = new ByteArrayContent(memoryStream.ToArray());
+
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
+            result.StatusCode = HttpStatusCode.OK;
+
+            return result;
         }
     }
 }
