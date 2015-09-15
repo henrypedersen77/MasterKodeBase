536fd2664ffde8682b96bd9804bf8f867e636fc5
henrypedersen77 <henrypedersen77@gmail.com>
2015-09-15 20:11:03 +0200

    About og Index

---
+++ webshop_mvc_basis/Scripts/About.js
@@ -0,0 +1,8 @@
+﻿(function () {
+    angular.module('About', [])
+    .controller('AboutCtrl', [function () {
+        this.qoute = "We know what we are, but not what we may be .";
+        this.author = "Abraham Lincoln";
+        console.log('MainCtrl has been created');
+    }]);
+})();
---
+++ webshop_mvc_basis/Scripts/Index.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    angular.module('Index', [])
+    .controller('IndexCtrl', ['$http', '$window', function ($http, $window) {
+        var self = this;
+        self.products = [];
+        $http.get('/api/product').then(function (result) {
+            self.products = result.data;
+        });
+
+        self.getImageLink = function (p) {
+            return '../Content/Images/Products/' + p;           
+        }
+
+        self.rederect = function (p) {
+            
+                $window.location.href = '../Pages/Product.html?Id=' + p;
+            
+        }
+    }]);
+})();
