3891a811007e72d6f77a1bebdd5c45c020c59919
henrypedersen77 <henrypedersen77@gmail.com>
2015-09-15 21:21:00 +0200

    Login

---
+++ webshop_mvc_basis/Scripts/Login.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    angular.module('Login', [])
+    .controller('LoginCtrl', ['$http', '$window', function ($http, $window) {
+        var self = this;
+        self.login = function () {
+            $http.post('/api/login', { login: self.user, password: self.password }).then(function (result) {
+                $window.location.href = '/Pages/Index.html';
+            }, function (errResponse) {
+                self.errorMsg = errResponse.data;
+            });
+        }
+    }]);
+})(); 
