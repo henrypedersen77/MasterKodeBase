0258f8b548f1d3eb06fe2737f3ceadaf6947f1a3
henrypedersen77 <henrypedersen77@gmail.com>
2015-09-18 15:06:19 +0200

    Register

---
+++ webshop_mvc_basis/Scripts/Register.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    angular.module('Register', [])
+    .controller('RegisterCtrl', ['$http', '$window', function ($http, $window) {
+        var self = this;
+        self.message = '';
+        self.username;
+        self.password;
+        self.confirmpassword;
+        self.firstname;
+        self.lastname;
+        self.address;
+        self.postalcode;
+        self.register = function () {
+            if (typeof self.password != 'undefined' && self.password === self.confirmpassword) {
+                $http.post('/api/register', {
+                    login: self.username,
+                    password: self.password,
+                    user: {
+                        FirstName: self.firstname,
+                        LastName: self.lastname,
+                        Address: self.address,
+                        PostalCode: self.postalcode
+                    }
+                }).then(function (result) {
+                    $window.location.href = '/Pages/Index.html';
+                }, function (errResponse) {
+                    self.message = errResponse.data;
+                });
+            } else {
+                self.message = 'The passwords do not match!';
+            }
+        };
+    }]);
+})();
