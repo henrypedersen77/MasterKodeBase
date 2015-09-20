b7539bc0bd2036fb53dc956bbd160adfdbf1e318
henrypedersen77 <henrypedersen77@gmail.com>
2015-09-20 22:25:36 +0200

    Rettelse til ManageProduct

--- webshop_mvc_basis/Scripts/ManageProducts.js
+++ webshop_mvc_basis/Scripts/ManageProducts.js
@@ -2,18 +2,18 @@
     angular.module('ManageProduct', [])
     .controller('ManageProductCtrl', ['$http', '$location', function ($http, $location) {
         var self = this;
-        self.producttypeids = [];
         var id = $location.search().Id;
         if (id != 'undefined') {
             $http.get('/api/product/' + id).then(function (result) {
                 self.product = result.data;
+                getproducttype();
             });
         }
         self.saveproduct = function () {
             $http.post('/api/product', {
                 ID: self.product.ID,
                 Name: self.product.Name,
-                TypeID: self.product.TypeID,
+                TypeID: self.TypeID.ID,
                 Price: self.product.Price,
                 Description: self.product.Description,
                 Image: self.product.Image
@@ -29,9 +29,16 @@
         });
         $http.get('/api/producttype').then(function (result) {
             self.producttypes = result.data;
-            for (index = 0; index < self.producttypes.length; ++index) {
-                self.producttypeids.push(self.producttypes[index].ID);
-            }
         });
+        getproducttype = function () {
+            $http.get('/api/producttype').then(function (result) {
+                self.producttypes = result.data;
+                for (index = 0; index < self.producttypes.length; ++index) {
+                    if (self.producttypes[index].ID === self.product.TypeID) {
+                        self.TypeID = self.producttypes[index];
+                    }
+                }
+            });
+        };
     }]);
 })();
