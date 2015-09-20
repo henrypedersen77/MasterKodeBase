d4606dd74a3e35d4d889b074a04dfc0e824fa794
henrypedersen77 <henrypedersen77@gmail.com>
2015-09-20 14:54:46 +0200

    ManageProductType

---
+++ webshop_mvc_basis/Scripts/ManageProductTypes.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    angular.module('ManageProductTypes', [])
+    .controller('ManageProductTypesCtrl', ['$http', '$location', function ($http, $location) {
+        var self = this;
+        var id = $location.search().Id;
+        if (id != 'undefined') {
+            $http.get('/api/producttype/' + id).then(function (result) {
+                self.producttype = result.data;
+            });
+        }
+        self.saveproducttype = function () {
+            $http.post('/api/producttype', { ID: self.producttype.ID, Name: self.producttype.Name }).then(
+            function (result) {
+                self.message = 'the producttype have been saved!';
+            }, function (result) {
+                self.message = 'error: ' + result.data;
+            });
+        };
+    }]);
+})();
