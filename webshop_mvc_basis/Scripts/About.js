bfd0b753a4bf0d8b856bf54088f98346817e501d
henrypedersen77 <henrypedersen77@gmail.com>
2015-10-06 20:05:09 +0200

    About

---
+++ webshop_mvc_basis/Scripts/About.js
@@ -0,0 +1,26 @@
+﻿var app = app || {};
+
+app.AboutModel = Backbone.Model.extend({
+    defaults: {
+        quote: '',
+        author: ''
+    }
+});
+
+var data = new app.AboutModel({ quote: 'We know what we are, but not what we may be .', author: 'Abraham Lincoln' });
+
+app.AppView = Backbone.View.extend({
+    el: '#container',
+    aboutTemplate: _.template($('#about-template').html()),
+    render: function () {
+        this.$el.html(this.aboutTemplate(data.toJSON()));
+        return this;
+    },
+    initialize: function () {
+        this.render();
+    }
+});
+
+$(function () {
+    new app.AppView();
+});
