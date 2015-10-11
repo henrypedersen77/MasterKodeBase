b738915731d7203acae91474301a56c82d824ad0
henrypedersen77 <henrypedersen77@gmail.com>
2015-10-11 22:10:34 +0200

    Index

---
+++ webshop_mvc_basis/Scripts/Index.js
@@ -0,0 +1,46 @@
+﻿var app = app || {};
+
+app.Products = Backbone.Collection.extend({
+    url: '/api/Product',
+    reset: true
+});
+
+var allProducts;
+
+//app.Router = Backbone.Router.extend({
+//    routes: {
+//        "Pages/Product.html/:id": "viewProduct"
+//    },
+//    viewProduct: function(id){
+//        this.navigate("Pages/Product.html/ + id", { trigger: true });
+//    }
+//});
+
+app.AppView = Backbone.View.extend({
+    el: '#ContentPlaceHolder1_pnlProducts',
+    //events: {'click': 'handleClick'},
+    indexTemplate: _.template($('#index-template').html()),
+    render: function () {
+        var self = this;
+        allProducts = new app.Products();
+        allProducts.fetch();
+        allProducts.on('all', function (data) {
+            self.$el.html(self.indexTemplate(allProducts.toJSON()));
+        });
+        return this;
+    },
+    initialize: function () {
+        this.render();
+    },
+    //handleClick: function (e) {
+    //    e.preventDefault(); 
+    //    var id = $(e.target).data("id");
+    //    //"redirect" til /pages/product.html?id=x
+    //    var redirect = new app.Router();
+    //    redirect.viewProduct(id);
+    //}
+});
+
+$(function () {
+    new app.AppView();
+});
---
+++ webshop_mvc_basis/Scripts/Product.js
@@ -0,0 +1,15 @@
+﻿var app = app || {};
+
+app.Router = Backbone.Router.extend({
+    routes: {
+        ":id": "viewProduct"
+    },
+    viewProduct: function(id){
+        var test = id;
+    }
+});
+
+$(function () {
+    new app.Router();
+    Backbone.history.start();
+});
