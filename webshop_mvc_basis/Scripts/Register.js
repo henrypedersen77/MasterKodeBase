ad23f6194e0d3ab6f2dda204c64d89e6be7b5ad7
henrypedersen77 <henrypedersen77@gmail.com>
2015-10-12 15:22:00 +0200

    Register

---
+++ webshop_mvc_basis/Scripts/Register.js
@@ -0,0 +1,47 @@
+﻿var app = app || {};
+
+app.RegisterModel = Backbone.Model.extend({
+    url: '/api/Register',
+    defaults: {
+        login: '',
+        password: '',
+        user: {
+            FirstName: '',
+            LastName: '',
+            Address: '',
+            PostalCode: 0000
+        }
+    }
+});
+
+app.AppView = Backbone.View.extend({
+    el: '#btnRegister',
+    events: { 'click': 'handleClick' },
+    handleClick: function (e) {
+        if ($('#txtPassword').val() === $('#txtConfirmPassword').val()) {
+            var data = new app.RegisterModel({
+                login: $('#txtUserName').val(),
+                password: $('#txtPassword').val(),
+                user: {
+                    FirstName: $('#txtFirstName').val(),
+                    LastName: $('#txtLastName').val(),
+                    Address: $('#txtAddress').val(),
+                    PostalCode: $('#txtPostalCode').val()
+                }
+            });
+
+            data.save().done(function (response) {
+                //redirect til index siden
+                window.location("/Pages/Index.html");
+            }).fail(function (response) {
+                $('#statusmessage').html("Error registering user! " + response);
+            });
+        } else {
+            $('#statusmessage').html("The passwords do not match!");
+        }
+    }
+});
+
+$(function () {
+    new app.AppView();
+});
