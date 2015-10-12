a5e831446c1a939d675ad21f4119d2f36a90d1ed
henrypedersen77 <henrypedersen77@gmail.com>
2015-10-12 15:49:34 +0200

    Login

---
+++ webshop_mvc_basis/Scripts/Login.js
@@ -0,0 +1,31 @@
+﻿var app = app || {};
+
+app.RegisterModel = Backbone.Model.extend({
+    url: '/api/Login',
+    defaults: {
+        login: '',
+        password: ''
+    }
+});
+
+app.AppView = Backbone.View.extend({
+    el: '#btnLogin',
+    events: { 'click': 'handleClick' },
+    handleClick: function (e) {
+        var data = new app.RegisterModel({
+            login: $('#txtUserName').val(),
+            password: $('#txtPassword').val(),
+        });
+
+        data.save().done(function (response) {
+            //redirect til index siden
+            window.location("/Pages/Index.html");
+        }).fail(function (response) {
+            $('#statusmessage').html("Invalid login!");
+        });
+    }
+});
+
+$(function () {
+    new app.AppView();
+});
