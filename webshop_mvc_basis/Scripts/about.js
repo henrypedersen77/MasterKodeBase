89ae73f32b914a84af6a94ff3453f5e5bb160549
henrypedersen77 <henrypedersen77@gmail.com>
2015-10-24 18:21:20 +0200

    About

---
+++ webshop_mvc_basis/Scripts/about.js
@@ -0,0 +1,10 @@
+﻿app = Ember.Application.create({ rootElement: "#ember-app" });
+
+app.IndexRoute = Ember.Route.extend({
+    model: function () {
+        return {
+            qoute: "We know what we are, but not what we may be .",
+            author: 'Abraham Lincoln'
+        }
+    }
+});
