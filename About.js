80229c5599859b5232cc0ac82d824c3b5a0d9a80
bjarnepedersen <bjarne@fampedersen.net>
2015-10-25 19:53:57 +0100

    Commit

---
+++ About.js
@@ -0,0 +1,35 @@
+﻿var app = app || {};
+
+app.About = Backbone.Model.extend({
+    url: '/about/1',
+    default: {
+        quote: '',
+        author: ''
+    },
+});
+
+
+app.AboutView = Backbone.View.extend({
+    el: '#about-view',
+    template: _.template($('#about-tmpl').html()),
+
+    initialize: function () {
+        this.model.fetch();
+        this.render();
+    },
+    render: function(){
+        var html = this.template(this.model.toJSON());
+        this.$el.html(html);
+        return this;    
+    }
+});
+
+var about = new app.About({
+    quote: "We know what we are, but not what we may be .",
+    author: "Abraham Lincolntest"
+}
+);
+
+$(function () { 
+    new app.AboutView({ model: about });
+});
